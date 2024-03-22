4a44760b571fba2d1cfb8e07ffea0550466898d4
cdsl3r <cdsl3r@no-reply.com>
2024-03-22 06:45:28 +0500

    added basic functionality

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include <panda3d/pandaFramework.h>
+#include <panda3d/pandaSystem.h>
+
+int main(int argc, char* argv[])
+{
+    PandaFramework fr;
+    fr.open_framework(argc, argv);
+
+    fr.set_window_title("Hello ;)");
+    WindowFramework *window = fr.open_window();
+
+
+    fr.main_loop();
+    fr.close_framework();
+
+    return 0;
+}
